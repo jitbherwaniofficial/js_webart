2aa1250d26b85e3ab68d4cd99a6e81150be02492
jitbherwaniofficial <jitbherwani92@gmail.com>
2024-06-17 01:59:23 +0530

    Initials 18

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ const hero = document.querySelector(".hero_section")
 const header = document.querySelector(".header")
 window.onscroll = function () {
   let top = window.scrollY;
-  if (top > 620) {
+  if (top > 600) {
     header.classList.add("active");
   } else {
     header.classList.remove("active");
