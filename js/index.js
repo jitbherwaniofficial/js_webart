a544fed980909f538a1beab2c91303e9baa8ab93
jitbherwaniofficial <jitbherwani92@gmail.com>
2024-06-17 03:05:12 +0530

    Initials 19

--- js/index.js
+++ js/index.js
@@ -2,11 +2,11 @@ console.log("Hare Krishna");
 
 const hero = document.querySelector(".hero_section")
 const header = document.querySelector(".header")
-window.onscroll = function () {
-  let top = window.scrollY;
-  if (top > 600) {
-    header.classList.add("active");
-  } else {
-    header.classList.remove("active");
-  }
-};
+// window.onscroll = function () {
+//   let top = window.scrollY;
+//   if (top > 600) {
+//     header.classList.add("active");
+//   } else {
+//     header.classList.remove("active");
+//   }
+// };
